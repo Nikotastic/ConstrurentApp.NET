846ef2f7b90aa3ae0f4860cba5310d9caef8e8de
Nikol Velasquez <velasqueznikol10@gmail.com>
2025-10-29 11:32:22 -0500

    feat(core): define IProductRepository interface for product data operations

---
+++ Firmness.Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Firmness.Core.Entities;
+
+namespace Firmness.Core.Interfaces;
+
+// interface for product repository
+public interface IProductRepository
+{
+    // CRUD operations
+    Task<Product?> GetByIdAsync(Guid id);
+    Task<IEnumerable<Product>> GetAllAsync();
+    Task<IEnumerable<Product>> SearchAsync(string? query);
+    Task AddAsync(Product product);
+    Task UpdateAsync(Product product);
+    Task DeleteAsync(Guid product);
+}
