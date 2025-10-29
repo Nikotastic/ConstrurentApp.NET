fa3b05be2c5c1b372a9de434e5c48395568fdad4
Nikol Velasquez <velasqueznikol10@gmail.com>
2025-10-28 23:09:48 -0500

    "feat(core): add base entities Person and User"

---
+++ Firmness.Core/Entities/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace Firmness.Core.Entities;
+
+// abstract class for all persons
+public abstract class Person
+{
+    public Guid Id { get; protected set; } = Guid.NewGuid();
+    public string FirstName { get; set; } = String.Empty;
+    public string LastName { get; set; } = String.Empty;
+    public string? Email { get; set; }
+    
+    // constructor 
+    protected Person() { }
+    
+    // constructor with parameters
+    protected Person(string firstName, string lastName, string? email)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+    }
+    
+    // property for full name
+    public string FullName => $"{FirstName} {LastName}".Trim();
+}
---
+++ Firmness.Core/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿namespace Firmness.Core.Entities;
+
+// class for users heritage from Person
+public class User : Person
+{
+    // properties
+    public string Username { get; set; } = String.Empty;
+    public string PasswordHash { get; set; } = String.Empty;
+    public bool IsActive { get; set; } = true;
+    public DateTime? LastLoginDate { get; set; }
+    public DateTime? LastLogoutDate { get; set; }
+    public DateTime? LastPasswordChangeDate { get; set; }
+
+    
+    // constructor 
+    protected User() : base()
+    {
+    }
+
+    // constructor with parameters
+    public User(string firstName, string lastName, string username, string passwordHash, DateTime? dateOfBirth = null,
+        string? email = null)
+        : base(firstName, lastName, email)
+    {
+        Username = username;
+        PasswordHash = passwordHash;
+    }
+}
