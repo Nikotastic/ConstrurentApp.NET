bb2115bf4177311e4853e23046cb61f86f9d81d6
Nikol Velasquez <velasqueznikol10@gmail.com>
2025-11-01 15:55:58 -0500

    chore(infrastructure): add DesignTimeDbContextFactory for EF Core tooling
    - Added DesignTimeDbContextFactory to enable 'dotnet ef' operations.
    - Configures ApplicationDbContext using appsettings or environment variables.
    - Supports EF Core tooling both locally and inside containers.

---
+++ Firmness.Infrastructure/DesignTimeDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Firmness.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Firmness.Infrastructure;
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var conn = Environment.GetEnvironmentVariable("CONN_STR")
+                   ?? "Host=postgres;Port=5432;Database=FirmnessDB;Username=postgres;Password=niko";
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        optionsBuilder.UseNpgsql(conn);
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
